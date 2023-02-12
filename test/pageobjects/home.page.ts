08eb81ab6051016cb71a0d597be71293f1e5ae46
Georgekaseri <georgekaseri22@gmail.com>
2023-02-12 18:44:33 +0000

    added helper

--- test/pageobjects/home.page.ts
+++ test/pageobjects/home.page.ts
@@ -1,15 +1,19 @@
 class HomePage {
-  get serviceNavigation() {
-    return $$("//i[@class='ii-1odz88f']")[0];
+  get gettradingAccount() {
+    return $$(".ii-1ai24k")[0];
   }
 
   async clickOnServiceNavigation() {
-    const el = $$("//i[@class='ii-1odz88f']")[0];
+    const el = $$(".ii-1odz88f")[0];
     let clickable = await el.isClickable();
 
     // wait for element to be clickable
     await browser.waitUntil(() => el.isClickable());
     return await el.click();
+  }
+
+  async clickOnTradingAccount() {
+    await this.gettradingAccount.click();
   }
 }
 export default new HomePage();
--- test/specs/e2e.ts
+++ test/specs/e2e.ts
@@ -1,10 +1,9 @@
 import chai, { expect } from "chai";
-
-import LoginPage from "../pageobjects/login.page";
 import HomePage from "../pageobjects/home.page";
+import LoginPage from "../pageobjects/login.page";
 import AlertPage from "../pageobjects/alert.page";
 import testdata from "../data/testData.json";
-import homePage from "../pageobjects/home.page";
+import { waitForTextToAppear, waitForVisible } from "../helper/helpers";
 
 describe("should test Interactive Investor application", async () => {
   before(() => {
@@ -12,19 +11,30 @@ describe("should test Interactive Investor application", async () => {
   });
 
   it("should handle the alert", async () => {
+    waitForVisible(".chakra-modal__content-container ii-v9b9hc");
     await AlertPage.clickOnAlertButton();
   });
-  it("should get the correct page titel", async () => {
+
+  it("should verify the current url", async () => {
+    expect("a[href=https://www.ii.co.uk/]");
+  });
+
+  it("should get the correct page title", async () => {
+    waitForVisible(".text-display");
     const title = await browser.getTitle();
     expect(title).to.equal(testdata.title);
   });
+});
 
+describe("should test Service dropdown in the top nav bar", async () => {
   it("should click on the “Services” dropdown in the top nav bar,", async () => {
-    browser.pause(5000);
     await HomePage.clickOnServiceNavigation();
+    waitForTextToAppear("//a[text()='Trading Account']", "Trading Account");
+  });
 
-    const elem = await $("//a[text()='Trading Account']");
-    await elem.waitForDisplayed({ timeout: 3000 });
-    await (await $("//a[text()='Trading Account']")).click();
+  it("should verify that user landed on the expected URL", async () => {
+    await HomePage.clickOnTradingAccount();
+    waitForTextToAppear("h1]", "Trading Account");
+    expect("a[href=https://www.ii.co.uk/ii-accounts/trading-account]");
   });
 });
